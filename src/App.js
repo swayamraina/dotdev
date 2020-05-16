8163493e461c6a269731bb1750edf918284d4df0
swayamraina <swayamraina@gmail.com>
2020-05-16 14:00:01 +0530

    add landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import Home from './containers/Home.js'
+import LandingPage from './containers/LandingPage.js';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App () {
+
+    const [ landingPage, setLandingPage ] = useState(true);
+
+    const moveToHome = () => {
+        setLandingPage(false);
+    }
+
+    const render = () => {
+        return landingPage ? <LandingPage onClick={moveToHome} /> : <Home />
+    }
+    
+    return (
+       render()
+    );
+
 }
 
 export default App;
---
+++ src/containers/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function LandingPage (props) {
+    
+    return (
+        <div onClick = {props.onClick}>
+            LandingPage
+        </div>
+    );
+
+}
+
+export default LandingPage;

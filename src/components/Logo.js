a56dfbd7375fe5a4df441772607bad0cca7fb746
swayamraina <swayamraina@gmail.com>
2020-05-17 23:48:53 +0530

    moved logo to new component

---
+++ src/components/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../styles/Logo.css'
+
+
+
+function Logo () {
+
+    return (
+        <div className="arrow">
+            <div className="arrow-left"></div>
+            <div className="arrow-right"></div>
+        </div>
+    )
+}
+
+export default Logo;

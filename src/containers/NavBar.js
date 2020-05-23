11acaa545e8bec38b9d633c8b4b31be8cb8b8098
swayamraina <swayamraina@gmail.com>
2020-05-23 19:11:55 +0530

    added nav-bar component

---
+++ src/containers/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../styles/NavBar.css'
+
+
+
+function NavBar () {
+
+    return (
+        <div>
+            <div className="nav-bar">
+                <div className="home-logo">&lt; ! swayamraina &gt;</div>
+                <Link className="nav-link" to='/'>Home</Link>
+                <Link className="nav-link" to='/blogs'>Blog</Link>
+                <Link className="nav-link" to="/projects">Projects</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+            </div>
+        </div>
+    );
+
+}
+
+export default NavBar;

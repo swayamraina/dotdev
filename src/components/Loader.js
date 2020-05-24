de3cf56c8df34763cbdea531d9ae89e756c3f260
swayamraina <swayamraina@gmail.com>
2020-05-24 15:49:41 +0530

    added loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../styles/Loader.css'
+
+
+
+function Loader (props) {
+
+    return (
+        <div className="loader" />
+    );
+
+}
+
+export default Loader;

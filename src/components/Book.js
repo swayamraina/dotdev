30872c85844a6d341ba729129b151ad59e479028
swayamraina <swayamraina@gmail.com>
2020-05-26 01:09:50 +0530

    added book component

---
+++ src/components/Book.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import '../styles/Book.css'
+
+
+
+function Book (props) {
+
+    return (
+        <div className="book">
+            <div className="book-cover" style={{backgroundImage: `url(${props.cover})`}}/>
+            <ul className="book-desc">
+                <li className="book-title"> <b>Title:</b> {props.title}</li> <br/>
+                <li className="book-author"> <b>Author:</b> {props.author}</li> <br/><br/><br/>
+                <li className="blog-content" dangerouslySetInnerHTML={{__html: props.description}} />
+            </ul>
+        </div>
+    );
+
+}
+
+export default Book;

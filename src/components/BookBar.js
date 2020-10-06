f2033bd2a4f3cbc55a34e6aeea88c96bc17f3756
swayamraina <swayamraina@gmail.com>
2020-10-06 23:25:35 +0530

    added component

---
+++ src/components/BookBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import '../styles/BookBar.css'
+
+
+
+function BookBar (props) {
+    return (
+        <div >
+            hello
+        </div>
+    );
+
+}
+
+export default BookBar;

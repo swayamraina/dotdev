293946c052a60d66c2fb8ac060cddd9691044311
swayamraina <swayamraina@gmail.com>
2020-10-06 23:26:07 +0530

    add conditional element

--- src/components/PinnedBook.js
+++ src/components/PinnedBook.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
+import BookBar from './BookBar'
+
 import '../styles/PinnedBook.css'
 
 
 
 function PinnedBook (props) {
 
+    const pagesAvailable = props.total && props.read
+    let bb = pagesAvailable ? <BookBar total="" read="" /> : <br/>;
+
     return (
         <div className="pinned-book">
             <div className="book-cover" style={{backgroundImage: `url(${props.cover})`}}/>
+            { bb }
         </div>
     );
 

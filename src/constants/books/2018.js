cfb5a9a985829c74ee1650b4f468c78fce80ed75
swayamraina <swayamraina@gmail.com>
2020-10-12 19:48:20 +0530

    refactor book storage

---
+++ src/constants/books/2018.js
@@ -0,0 +1,32 @@
+
+export const _2018 = [
+
+    {
+        "name": "Alchemist",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/410llGwMZGL._SX328_BO1,204,203,200_.jpg",
+        "link": "",
+        "rating": 5, "read": 179, "total": 179,
+    },
+
+    {
+        "name": "Kite Runner",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/51vRNqL61aL._SX318_BO1,204,203,200_.jpg",
+        "link": "",
+        "rating": 4, "read": 372, "total": 372,
+    },
+
+    {
+        "name": "To Kill A Mockingbird",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/51Z9p5AecCL._SX307_BO1,204,203,200_.jpg",
+        "link": "",
+        "rating": 4, "read": 296, "total": 296,
+    },
+
+    {
+        "name": "REVA EV",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/412xYouzyPL._SX323_BO1,204,203,200_.jpg",
+        "link": "",
+        "rating": 3.5, "read": 172, "total": 172,
+    },
+
+]
---
+++ src/constants/books/2019.js
@@ -0,0 +1,25 @@
+
+export const _2019 = [
+
+    {
+        "name": "Before I go Yo Sleep",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/51AX7bENbUL._SX315_BO1,204,203,200_.jpg",
+        "link": "",
+        "rating": 4, "read": 368, "total": 368,
+    },
+
+    {
+        "name": "Nikola Tesla",
+        "image": "https://m.media-amazon.com/images/I/51NA6SBuY2L.jpg",
+        "link": "",
+        "rating": 3.5, "read": 34, "total": 34,
+    },
+
+    {
+        "name": "Why I Killed Gandhi",
+        "image": "https://m.media-amazon.com/images/I/51M3qfxdlcL.jpg",
+        "link": "",
+        "rating": 3, "read": 4, "total": 4,
+    },
+
+]

1a581abf0ad828bf4cdee17f50c049703ede7892
swayamraina <swayamraina@gmail.com>
2020-10-20 23:51:21 +0530

    move to insource constants

---
+++ src/constants/engineering/2020.js
@@ -0,0 +1,24 @@
+
+export const _2020 = [
+
+    {
+        "title": "Data transfer beyond your application code",
+        "description": "",
+        "tags": [ "API", "Linux", "Socket" ],
+        "date": "19.07.2020",
+        "image": "",
+        "link": "/blogs/engineering/2020/linux-networking",
+        "read": 0,
+    },
+
+    {
+        "title": "",
+        "description": "",
+        "tags": [],
+        "date": "",
+        "image": "",
+        "link": "",
+        "read": 0,
+    },
+
+]

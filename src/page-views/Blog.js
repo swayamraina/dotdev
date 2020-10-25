dadb3089a0b8fd0ce61ba790974bf7f8e395104f
swayamraina <swayamraina@gmail.com>
2020-10-25 16:09:07 +0530

    update blog component

--- src/page-views/Blog.js
+++ src/page-views/Blog.js
@@ -1,48 +1,16 @@
 import React from 'react';
 
 import BlogCategory from './BlogCategory';
-
-import '../styles/Blog.css'
 import Book from '../components/Book';
+import { categories } from '../constants/categories';
+
+import '../styles/Blog.css';
+
 
 
 
 function Blog () {
-
-    const categories = [
-        {
-            "type": "engineering",
-            "title": "Engineering",
-            "description": "Truth can only be found in one place: the code.",
-            "link": "/blogs/engineering",
-        },
-        {
-            "type": "coding",
-            "title": "Coding",
-            "description": "An algorithm must be seen to be believed.",
-            "link": "/blogs/coding",
-        },
-        {
-            "type": "poetry",
-            "title": "Poetry",
-            "description": "Poetry is evidence of life. If your life is burning well, poetry is just the ash.",
-            "link": "/blogs/poetry",
-        },
-        {
-            "type": "history",
-            "title": "History",
-            "description": "History will be kind to me for I intend to write it.",
-            "link": "/blogs/history",
-        },
-        {
-            "type": "books",
-            "title": "Books",
-            "description": "That's the thing about books. They let you travel without moving your feet.",
-            "link": "/blogs/books",
-        },
-    ]
     
-
     return (
         <div>
             <div className="blog-desc">
@@ -74,6 +42,7 @@ function Blog () {
                                 link = {t.link}
                                 title = {t.title}
                                 description = {t.description}
+                                image = {t.image}
                             /> 
                     )
                 }

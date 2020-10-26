dff1faa078f590f75dfc71eb930679cfecc90c6b
swayamraina <swayamraina@gmail.com>
2020-10-26 12:13:42 +0530

    book links

--- src/constants/books/2019.js
+++ src/constants/books/2019.js
@@ -4,21 +4,26 @@ export const _2019 = [
     {
         "name": "Before I go Yo Sleep",
         "image": "https://images-na.ssl-images-amazon.com/images/I/51AX7bENbUL._SX315_BO1,204,203,200_.jpg",
-        "link": "",
+        "link": [
+            "https://www.flipkart.com/before-go-sleep/p/itmfbsd6fy6gzvys",
+            "https://www.amazon.in/Before-I-Go-Sleep-Watson/dp/0552164127",
+        ],
         "rating": 4, "read": 368, "total": 368,
     },
 
     {
         "name": "Nikola Tesla",
         "image": "https://m.media-amazon.com/images/I/51NA6SBuY2L.jpg",
-        "link": "",
+        "link": [],
         "rating": 3.5, "read": 34, "total": 34,
     },
 
     {
         "name": "Why I Killed Gandhi",
         "image": "https://m.media-amazon.com/images/I/51M3qfxdlcL.jpg",
-        "link": "",
+        "link": [
+            "https://www.amazon.in/Why-Killed-Gandhi-Nathuram-Godse-ebook/dp/B07W8T59PD",
+        ],
         "rating": 3, "read": 4, "total": 4,
     },
 
--- src/constants/books/2020.js
+++ src/constants/books/2020.js
@@ -4,29 +4,50 @@ export const _2020 = [
     {
         "name": "97 Things Java Programmers Should Know",
         "image": "https://images-na.ssl-images-amazon.com/images/I/516b9EbjBOL._SX332_BO1,204,203,200_.jpg",
-        "link": "",
+        "link": [
+            "https://www.amazon.in/Things-Every-Java-Programmer-Should/dp/1491952695",
+        ],
         "rating": "", "read": 64, "total": 259,
     },
 
     {
         "name": "Animal Farm",
         "image": "https://images-na.ssl-images-amazon.com/images/I/41bgiGplMlL._SX301_BO1,204,203,200_.jpg",
-        "link": "",
+        "link": [
+            "https://www.amazon.in/Animal-Farm-George-Orwell/dp/0143416316",
+            "https://www.flipkart.com/animal-farm/p/itmfbqreghbzkj7y",
+        ],
         "rating": 4.5, "read": 134, "total": 134,
     },
 
     {
         "name": "The Victory Project",
         "image": "https://images-na.ssl-images-amazon.com/images/I/41jZMl+QUiL._SX297_BO1,204,203,200_.jpg",
-        "link": "",
-        "rating": "", "read": 121, "total": 307,
+        "link": [
+            "https://www.amazon.in/Victory-Project-Steps-Peak-Potential/dp/0670093254",
+            "https://www.flipkart.com/victory-project-six-steps-peak-potential/p/itm6f35b51c13f18",
+        ],
+        "rating": "", "read": 225, "total": 307,
     },
 
     {
         "name": "Asura",
         "image": "https://images-na.ssl-images-amazon.com/images/I/61zTKrAg6WL._SX322_BO1,204,203,200_.jpg",
-        "link": "",
+        "link": [
+            "https://www.amazon.in/Asura-Vanquished-Story-Ravana-People/dp/938157605X",
+            "https://www.flipkart.com/asura/p/itmetjhxgnfbfahf",
+        ],
         "rating": "", "read": 245, "total": 504,
+    },
+
+    {
+        "name": "Man called Ove",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/519L6FBdlXL._SX326_BO1,204,203,200_.jpg",
+        "link": [ 
+            "https://www.amazon.in/Man-Called-Ove-life-affirming-bestseller/dp/1444775812",
+            "https://www.flipkart.com/man-called-ove/p/itmfbte7tbdwukcd"
+        ],
+        "rating": "", "read": 245, "total": 368,
     },
 
 ]

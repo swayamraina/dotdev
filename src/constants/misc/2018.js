b515c0d78b98574a3ffe063326e106b7e2f3a139
swayamraina <swayamraina@gmail.com>
2020-10-27 20:38:19 +0530

    add links

---
+++ src/constants/misc/2018.js
@@ -0,0 +1,14 @@
+
+export const _2018 = [
+
+    {
+        "title": "How can you improve your code quality using dependency injection",
+        "description": "",
+        "tags": [ "JAVA", "TDD", "Memory Management" ],
+        "date": "23.10.2018",
+        "image": "https://blog.semmle.com/static/f0b9db11df37d15e8c9a8a918fd44327/4416c/code_quality_1.png",
+        "link": "/blogs/misc/2018/dependency-injection",
+        "read": 0,
+    },
+
+]

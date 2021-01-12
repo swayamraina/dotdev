4695e4a6b0c7d5b4e3a83abac9087312ec9a3339
swayamraina <swayamraina@gmail.com>
2021-01-12 20:18:41 +0530

    book: 100 baggers

---
+++ src/constants/books/2021.js
@@ -0,0 +1,12 @@
+
+export const _2021 = [
+
+    {
+        "name": "100 Baggers",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/4125CeN5OML._SX331_BO1,204,203,200_.jpg",
+        "link": [
+            "https://www.amazon.in/100-Baggers-Stocks-100-1/dp/1621291650",
+        ],
+    },
+
+]

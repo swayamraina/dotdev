970557d19c6143af59b7e291f5a035fd6f87d3fd
swayamraina <swayamraina@gmail.com>
2021-01-28 00:42:00 +0530

    alamanac of naval ravikant

--- src/constants/books/2021.js
+++ src/constants/books/2021.js
@@ -9,4 +9,20 @@ export const _2021 = [
         ],
     },
 
+    {
+        "name": "Almanac of Naval Ravikant",
+        "image": "https://m.media-amazon.com/images/I/31EQXd8E9eL.jpg",
+        "link": [
+            "https://www.amazon.in/Almanack-Naval-Ravikant-Wealth-Happiness-ebook/dp/B08FF8MTM6",
+        ],
+    },
+
+    {
+        "name": "The Last Question",
+        "image": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1274107046l/4808763.jpg",
+        "link": [
+            "https://www.amazon.com/Last-Question-When-World-Ends/dp/1884214495",
+        ],
+    },
+
 ]
